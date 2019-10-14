1a752d3677c741bc46c547ba3bd43ee65910dbff
Alex Leybourne <alexleybourne1@gmail.com>
2019-10-14 11:27:19 +1000

    Finished data entry and show all

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,9 +1,21 @@
 class EnquiriesController < ApplicationController
+  
+  def index
+    @enquiries = Enquiry.all
+  end
+  
   def new
     @enquiry = Enquiry.new
   end
 
   def create
-    render plain: params
+    @enquiry = Enquiry.create(enquiry_params)
   end
+
+  private
+
+  def enquiry_params
+    params.require(:enquiry).permit(:name, :email, :phone, :message)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'enquiries', to: "enquiries#index"
   get 'enquiries/new'
   post 'enquiries', to: "enquiries#create"
 
